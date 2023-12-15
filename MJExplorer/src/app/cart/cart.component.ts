fa182b2e012ff7727248bc7b4b90ecc0151dabac
ankitgoyani <ankit.goyani@bluecypress.io>
2023-12-15 18:15:18 -0500

    updated cart page

--- MJExplorer/src/app/cart/cart.component.ts
+++ MJExplorer/src/app/cart/cart.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Book } from '../book-detail/book-detail.component';
+import { CartService } from '../utils/cart.service';
+import { Router } from '@angular/router';
+import { Metadata } from '@memberjunction/core';
+import { PurchaseDetailEntity, PurchaseEntity } from 'mj_generatedentities';
+import { SharedService } from '../utils/shared-service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +12,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  cartItems!: Book[];
+  grandTotal: number = 0;
 
+  constructor(private cartService: CartService, private router: Router, private sharedService: SharedService) { }
+
+  ngOnInit() {
+    this.cartService.cartItems$.subscribe((items) => {
+      this.cartItems = items;
+      this.calculateGrandTotal();
+      if (!items.length) {
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+
+  async checkout() {
+    const md = new Metadata();
+    if (!md.CurrentUser.LinkedEntityID) {
+      this.router.navigate(['/cart']);
+    } else {
+      const purchaseEntity = <PurchaseEntity>await md.GetEntityObject('Purchases');
+      purchaseEntity.NewRecord();
+      purchaseEntity.Date = new Date();
+      purchaseEntity.PersonID = md.CurrentUser.LinkedEntityID;
+      purchaseEntity.GrandTotal = this.grandTotal;
+      purchaseEntity.Save();
+      for (let i = 0; i < this.cartItems.length; i++) {
+        const book: Book = this.cartItems[i];
+        const purchaseDetailEntity = <PurchaseDetailEntity>await md.GetEntityObject('Purchase Details')
+        purchaseDetailEntity.NewRecord();
+        purchaseDetailEntity.PurchaseID = purchaseEntity.ID;
+        purchaseDetailEntity.BookID = book.ID;
+        purchaseDetailEntity.Quantity = book.Quantity;
+        purchaseDetailEntity.Amount = book.Amount;
+        purchaseDetailEntity.Total = this.calculateTotal(book);
+        purchaseDetailEntity.Discount = 0;
+        await purchaseDetailEntity.Save();
+      }
+      this.sharedService.DisplayNotification('Thank you for Shopping','success');
+      this.cartService.emptyCart();
+    }
+  }
+
+  removeItem(book: Book) {
+    this.cartService.removeFromCart(book);
+  }
+
+  decreaseQuantity(book: Book) {
+    this.cartService.decreaseQuantity(book);
+  }
+
+  increaseQuantity(book: Book) {
+    this.cartService.increaseQuantity(book);
+  }
+
+  calculateTotal(book: Book): number {
+    return this.cartService.calculateTotal(book);
+  }
+
+  calculateGrandTotal() {
+    this.grandTotal = this.cartService.calculateGrandTotal();
+  }
 }
